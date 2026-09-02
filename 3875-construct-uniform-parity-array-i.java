d0c5e9c8f19aebdae633e3259b58dedef6a5fc16
Sumit <sumitverma191105@gmail.com>
2026-09-02 06:28:34 +0530

    Solved Leetcode 3875

---
+++ 3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
+/*odd - even = odd
+even - odd = odd
+if all even then also true;
+if all odd then also true
+There is no possibility of becoming false 
+*/
